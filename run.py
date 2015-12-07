573d8a21d110fb45bde3f14a61b8b4ad6dad08c4
Josh Zheng <denial@gmail.com>
2015-12-07 01:55:33 -0800

    renamed record.py to run.py.

--- run.py
+++ run.py
@@ -1,20 +1,19 @@
+import os
+import pyaudio
+import wave
+import json
+
 from sys import byteorder
 from array import array
 from struct import pack
 from os.path import join, dirname
 
-import pyaudio
-import wave
-import os
-import requests, json, urllib, urllib2, base64
 from dotenv import load_dotenv
-
+from watson_developer_cloud import SpeechToTextV1 as SpeechToText
+from watson_developer_cloud import AlchemyLanguageV1 as AlchemyLanguage
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-
-from watson_developer_cloud import SpeechToTextV1 as SpeechToText
-
 
 THRESHOLD = 500
 CHUNK_SIZE = 1024
@@ -131,13 +130,16 @@ def transcribe_audio(path):
     speech_to_text = SpeechToText(username=username,
                                   password=password)
 
+    '''
     with open(join(dirname(__file__), path), 'rb') as audio_file:
         return speech_to_text.recognize(audio_file,
             content_type='audio/wav')
+    '''
+    return "it's nice to meet you Watson"
 
 
 def get_text_sentiment(apikey, text):
-
+    '''
     # Base AlchemyAPI URL for targeted sentiment call
     alchemy_url = "http://access.alchemyapi.com/calls/text/TextGetTextSentiment"
     
@@ -164,12 +166,15 @@ def get_text_sentiment(apikey, text):
         print "HTTP Status:", results.status_code, results.reason
         print "--"
         return
+    '''
+    print(json.dumps(alchemy_language.language(url=url), indent=2))
 
+    '''
     sentiment = response['docSentiment']['type']
     score = 0.
     if sentiment in ('positive', 'negative'):
         score = float(response['docSentiment']['score'])
-    
+    '''
     return sentiment, score
 
 
@@ -185,7 +190,8 @@ if __name__ == '__main__':
     print "Text: " + text
     print
     print "How do I feel about this?"
-    sentiment, score = get_text_sentiment(alchemy_api_key, text)
-    print sentiment, score
+    #sentiment, score = get_text_sentiment(alchemy_api_key, text)
+    #print sentiment, score
+    print(json.dumps(alchemy_language.language(text=text), indent=2))
 
 
