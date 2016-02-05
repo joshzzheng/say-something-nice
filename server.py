7089ff62adea7d67ae256693143870f0a8060efe
Yacine Rezgui <rezgui.y@gmail.com>
2016-02-05 09:19:17 +0900

    Add index page

--- server.py
+++ server.py
@@ -4,16 +4,16 @@ from watson_developer_cloud import AuthorizationV1 as WatsonAuthorization
 from watson_developer_cloud import SpeechToTextV1 as SpeechToText
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 auth = WatsonAuthorization(username=os.environ.get("BLUEMIX_USERNAME"),
                            password=os.environ.get("BLUEMIX_PASSWORD"))
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 @app.route("/token")
 def getToken():
