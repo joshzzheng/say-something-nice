6898bd2a6bc95b532f0c7f931d1f7fc37739034b
Yacine Rezgui <rezgui.y@gmail.com>
2016-02-05 12:55:39 +0900

    Add client side code

---
+++ static/client.js
@@ -0,0 +1,50 @@
+var state                   = 'loading'
+var model                   = 'en-US_BroadbandModel'
+var sttToken                = null
+var $button                 = document.getElementById('action')
+var defaultButtonClassName  = 'h3 btn btn-primary mb4'
+
+$button.addEventListener('click', function() {
+  switch(state) {
+    case 'ready':
+      state               = 'recording'
+      $button.textContent = 'Recording...'
+      WatsonSpeech.SpeechToText
+        .recognizeMicrophone({ token: sttToken, model: model })
+        .on('result', onResult)
+        .on('data', onFinish)
+      break
+
+    case 'recording':
+      state               = 'waiting'
+      $button.disable     = true
+      $button.textContent = 'Waiting final results...'
+      break
+  }
+})
+
+function onResult(results) {
+  document.getElementById('results').textContent = results.alternatives[0].transcript
+}
+
+function onFinish() {
+  state               = 'ready'
+  $button.disabled    = false
+  $button.textContent = 'Click to record'
+}
+
+function init() {
+  var xhr = new XMLHttpRequest()
+
+  xhr.addEventListener('load', function(evt) {
+    state               = 'ready'
+    sttToken            = evt.target.responseText
+    $button.disabled    = false
+    $button.textContent = 'Click to record'
+  })
+
+  xhr.open('GET', '/token')
+  xhr.send()
+}
+
+init()
