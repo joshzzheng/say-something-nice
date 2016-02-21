30d442950367c9351ca459c55c97ddb80bcb4f07
Yacine Rezgui <rezgui.y@gmail.com>
2016-02-21 07:46:38 -0800

    Add sentiment analysis

--- static/client.js
+++ static/client.js
@@ -2,37 +2,64 @@ var state                   = 'loading'
 var model                   = 'en-US_BroadbandModel'
 var sttToken                = null
 var $button                 = document.getElementById('action')
+var $results                = document.getElementById('results')
 var sttStream               = null
 var defaultButtonClassName  = 'h3 btn btn-primary mb4'
+var defaultResultsClassName = ''
 
 $button.addEventListener('click', function() {
   switch(state) {
     case 'ready':
       state               = 'recording'
       $button.textContent = 'Recording...'
+      $results.className  = ''
       sttStream = WatsonSpeech.SpeechToText
-        .recognizeMicrophone({ token: sttToken, model: model })
-        .on('result', onResult)
-        .on('data', onFinish)
+        .recognizeMicrophone({
+          token: sttToken,
+          model: model,
+          objectMode: true,
+          outputElement: '#results',
+        })
+
       break
 
     case 'recording':
-      sttStream.stop()
-      state               = 'waiting'
+      try {
+        sttStream.stop()
+      } catch(e) {
+
+      }
+
+      state               = 'analysing'
       $button.disable     = true
-      $button.textContent = 'Waiting final results...'
+      $button.textContent = 'Sentiment Analysing...'
+      sentimentAnalysis($results.textContent)
       break
   }
 })
 
-function onResult(results) {
-  document.getElementById('results').textContent = results.alternatives[0].transcript
-}
+function sentimentAnalysis(transcript) {
+  var xhr = new XMLHttpRequest()
 
-function onFinish() {
-  state               = 'ready'
-  $button.disabled    = false
-  $button.textContent = 'Click to record'
+  xhr.addEventListener('load', function(evt) {
+    state               = 'ready'
+    sentiment           = JSON.parse(evt.target.responseText).sentiment
+
+    if(sentiment === 'positive') {
+      $results.className = 'green'
+    } else if(sentiment === 'negative') {
+      $results.className = 'red'
+    }
+
+    $button.disabled    = false
+    $button.textContent = 'Click to record'
+  })
+
+  var formData = new FormData()
+  formData.append('transcript', transcript)
+
+  xhr.open('POST', '/sentiment')
+  xhr.send(formData)
 }
 
 function init() {
@@ -43,6 +70,7 @@ function init() {
     sttToken            = evt.target.responseText
     $button.disabled    = false
     $button.textContent = 'Click to record'
+    $results.className  = ''
   })
 
   xhr.open('GET', '/token')
